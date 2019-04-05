b3ad16ab9181ad8bd6e440f7dbc04c8a45bc7166
Your Name <you@example.com>
2019-04-05 09:48:44 -0700

    Just the beginning

---
+++ Wk5/skills/server/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const controller = require('controller')
+const PORT = 4000;
+app.listen(PORT,() => console.log(`Don't pick a fight with ${PORT}, he be crazy`
+    )
+);
