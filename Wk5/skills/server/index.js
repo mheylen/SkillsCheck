83e1cfffd789bc72635223b25e2634593a07885d
Your Name <you@example.com>
2019-04-05 11:30:10 -0700

    Starting on the App.js

--- Wk5/skills/server/index.js
+++ Wk5/skills/server/index.js
@@ -1,5 +1,16 @@
-const express = require('express')
-const controller = require('controller')
+const express = require('express');
+const controller = require('./controller');
+const app = express();
+const massive = require("massive");
+require("dotenv").config()
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('connected to db');
+});
+
+
+
 const PORT = 4000;
 app.listen(PORT,() => console.log(`Don't pick a fight with ${PORT}, he be crazy`
     )
